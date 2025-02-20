d9f615e99d440548f0915905da7d67ba0c8e735b
Carter Williams <carterww@hotmail.com>
2025-02-19 18:37:05 -0600

    OS specific config set automatically in config.mk

--- config.mk
+++ config.mk
@@ -7,12 +7,16 @@ VERSION = 0.9.2
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+UNAME := $(shell uname | awk '{print tolower($$0)}')
 
-# FreeBSD
-#X11INC = /usr/local/include
-#X11LIB = /usr/local/lib
+ifeq ($(UNAME),linux)
+	X11INC = /usr/X11R6/include
+	X11LIB = /usr/X11R6/lib
+endif
+ifeq ($(UNAME),freebsd)
+	X11INC = /usr/local/include
+	X11LIB = /usr/local/lib
+endif
 
 PKG_CONFIG = pkg-config
 
